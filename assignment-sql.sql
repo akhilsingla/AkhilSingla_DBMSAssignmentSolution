08578fd1ae73439d23aa140939f3293635ed7282
akhil singla <akhil.singla@logmein.com>
2021-11-14 14:40:24 +0530

    updated and added query

--- assignment-sql.sql
+++ assignment-sql.sql
@@ -1,6 +1,12 @@
 create database TravelOnTheGo;
 USE TravelOnTheGo;
 
+## for the normalization purpose divided PASSENGER table into 2 tables :- ROUTE_DETAILS, PASSENGER
+## PASSENGER table consists Passenger data with Foreign Key Route_Code mapped to Route_Code Primary key in ROUTE_DETAILS table
+## ROUTE_DETAILS table contains all the routes details, so we are saved from entering dulicate entries in PASSENGER table e.g
+## if there are 4 users travelling from "Mumbai" -> "Hyderabad", so need to enter duplicate entries for route i.e Category, Boarding City, Destination City, 
+## Distance, Bus type so data dulpication is saved.
+
 CREATE TABLE IF NOT EXISTS ROUTE_DETAILS(
 Route_Code int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 Category varchar(50),
@@ -21,17 +27,6 @@ Bus_Type varchar(50),
 Distance int,
 Price int
 ); 
-
-##INSERT INTO PASSENGER VALUES
-##("Sejal", "AC", "F", "Bengaluru", "Chennai", 350, "Sleeper"),
-##("Anmol", "Non-AC", "M", "Mumbai", "Hyderabad", 700, "Sitting"),
-##("Pallavi", "AC", "F", "Panaji", "Bengaluru", 600, "Sleeper"),
-##("Khusboo", "AC", "F", "Chennai", "Mumbai", 1500, "Sleeper"),
-##("Udit", "Non-AC", "M", "Trivandrum", "panaji", 1000, "Sleeper"),
-##("Ankur", "AC", "M", "Nagpur", "Hyderabad", 500, "Sitting"),
-##("Hemant", "Non-AC", "M", "panaji", "Mumbai", 700, "Sleeper"),
-##("Manish", "Non-AC", "M", "Hyderabad", "Bengaluru", 500, "Sitting"),
-##("Piyush", "AC", "M", "Pune", "Nagpur", 700, "SittiPASSENGERPASSENGERng");
 
 INSERT INTO ROUTE_DETAILS (Category, Boarding_City, Destination_City, Distance, Bus_Type) VALUES
 ("AC", "Bengaluru", "Chennai", 350, "Sleeper"),
@@ -72,7 +67,7 @@ INSERT INTO PRICE VALUES
 ("Sitting", 1500, 1860);
 
 ## query 3
-SELECT PASSENGER.Gender, COUNT(ROUTE_DETAILS.Distance) AS Distance_Travelled from Passenger INNER JOIN ROUTE_DETAILS ON 
+SELECT PASSENGER.Gender, COUNT(ROUTE_DETAILS.Distance) AS "Number of People Travelled Distance >= 600" from Passenger INNER JOIN ROUTE_DETAILS ON 
 PASSENGER.Route_Code = ROUTE_DETAILS.Route_Code WHERE ROUTE_DETAILS.Distance >= 600 GROUP BY PASSENGER.Gender;
 
 ##query 4
@@ -82,11 +77,20 @@ ROUTE_DETAILS.Bus_Type = PRICE.Bus_Type AND ROUTE_DETAILS.Distance = PRICE.Dista
 ##query 5
 SELECT PASSENGER.Passenger_name AS "Passengers Having Names Starts with 'S'" from PASSENGER where PASSENGER.Passenger_name LIKE "S%";
 
+## Price field for Kushboo will be null, since corresponding entry is missing in the Price table
+## there will be multiple entries for Ankur, Manish, Sejal since we have multiple entries for same bus type and distance.
+## Sleeper 350 770
+## Sleeper 350 434
+## Sitting 500 620
+## Sitting 500 620
 ##query 6
+SELECT PASSENGER.Passenger_name, ROUTE_DETAILS.Boarding_City, ROUTE_DETAILS.Destination_City, ROUTE_DETAILS.Bus_Type, PRICE.Price from (
+PASSENGER LEFT JOIN ROUTE_DETAILS ON PASSENGER.Route_Code = ROUTE_DETAILS.Route_Code) 
+LEFT JOIN PRICE ON PRICE.Bus_Type = ROUTE_DETAILS.Bus_Type AND PRICE.Distance = ROUTE_DETAILS.Distance;
 
-
+## this query will return empty result since there is no Passenger travelling 1000 KM's in Sitting bus 
 ##query 7
-SELECT PASSENGER.Passenger_name, ROUTE_DETAILS.Boarding_City, ROUTE_DETAILS.Destination_City, PRICE.Bus_Type, PRICE.Distance, PRICE.Price from( 
+SELECT PASSENGER.Passenger_name, ROUTE_DETAILS.Boarding_City, ROUTE_DETAILS.Destination_City, PRICE.Price from( 
 PASSENGER INNER JOIN ROUTE_DETAILS ON PASSENGER.Route_Code = ROUTE_DETAILS.Route_Code) 
 INNER JOIN PRICE ON PRICE.Bus_Type = ROUTE_DETAILS.Bus_Type AND PRICE.Distance = ROUTE_DETAILS.Distance WHERE PRICE.Bus_Type = "Sitting" AND PRICE.Distance=1000;
 
@@ -98,7 +102,8 @@ AND PRICE.Distance=
 (SELECT Distance from ROUTE_DETAILS WHERE ROUTE_DETAILS.Boarding_City IN("Bengaluru", "Panaji") AND ROUTE_DETAILS.Destination_City IN("Bengaluru", "Panaji"));
 
 ##query 9
-SELECT DISTINCT(Distance) from ROUTE_DETAILS ORDER BY DISTANCE DESC;
+SELECT DISTINCT(A.Distance) from (SELECT PASSENGER.Route_Code, ROUTE_DETAILS.Distance from PASSENGER INNER JOIN ROUTE_DETAILS ON PASSENGER.Route_Code = ROUTE_DETAILS.Route_Code) AS A
+ORDER BY A.DISTANCE DESC;
 
 ##query 10
 Select PASSENGER.Passenger_name, (ROUTE_DETAILS.Distance/(Select SUM(P.Distance) from (Select ROUTE_DETAILS.Distance from PASSENGER INNER JOIN ROUTE_DETAILS ON 
